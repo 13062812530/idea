9b6832db5c119207a87f213c9ff8539157b81546
admin <shengjie13062812530@aliyun.com>
2018-12-05 22:31:09 +0800

    更新bms-swagger拦截器处理

--- common/src/main/java/com/idea/common/Intercepotor/InterceptorConfig.java
+++ common/src/main/java/com/idea/common/Intercepotor/InterceptorConfig.java
@@ -9,14 +9,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
 @Configuration
 public class InterceptorConfig extends WebMvcConfigurationSupport {
     @Bean
-    public TokenInterceptor tokenInterceptor(){
+    public TokenInterceptor tokenInterceptor() {
         return new TokenInterceptor();
     }
 
 
     @Override
     protected void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(tokenInterceptor()).addPathPatterns("/**");
+        registry.addInterceptor(tokenInterceptor()).addPathPatterns("/**")
+                .excludePathPatterns("/swagger-resources/**", "/webjars/**", "/v2/**", "/swagger-ui.html/**");
         super.addInterceptors(registry);
     }
 
@@ -25,6 +26,10 @@ public class InterceptorConfig extends WebMvcConfigurationSupport {
         registry.addResourceHandler("/**")
                 .addResourceLocations("classpath:/META-INF/resource")
                 .addResourceLocations("classpath:/resource");
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
         super.addResourceHandlers(registry);
     }
 }
